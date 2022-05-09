b171c457f63090a4ed02508055c2ff3d37c54f12
irUdalova <irinabondarenko.mail@gmail.com>
2022-05-09 22:30:43 +0300

    feat: implement minimal scope

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// ====================render page app==============================
 class Keyboard {
+  isShifted = false;
+  isCapsLocked = false;
   constructor(element, layouts, lang) {
     this.element = element;
     this.layouts = layouts;
@@ -6,11 +9,70 @@ class Keyboard {
     this.renderGeneral();
     this.renderStatic();
     this.renderDynamic();
+    this.input = this.element.querySelector(".inp-text");
+    document.addEventListener("keydown", this.onKeyDown);
+    document.addEventListener("keyup", this.onKeyUp);
   }
 
   get layout() {
     return this.layouts[this.lang];
   }
+
+  onKeyDown = (event) => {
+    console.log(event);
+    const isInputFocused = event.target === this.input;
+    const keyEl = this.element.querySelector(`[data-code=${event.code}]`);
+    if (!keyEl) {
+      return;
+    }
+    keyEl.classList.add("pressed");
+    if (!isInputFocused && event.key.length === 1) {
+      this.input.value += event.key;
+      console.log(isInputFocused);
+    }
+
+    if (event.key === "Shift") {
+      this.isShifted = true;
+      this.element.classList.add("shifted");
+    }
+
+    if (event.key === "CapsLock") {
+      this.isCapsLocked = true;
+      this.element.classList.add("capslocked");
+    }
+
+    const keyValue = {
+      Tab: "\t",
+      ArrowLeft: "←",
+      ArrowUp: "↑",
+      ArrowDown: "↓",
+      ArrowRight: "→",
+    };
+
+    if (keyValue[event.key]) {
+      event.preventDefault();
+      this.input.value += keyValue[event.key];
+    }
+  };
+
+  onKeyUp = (event) => {
+    console.log(event);
+    const keyEl = this.element.querySelector(`[data-code=${event.code}]`);
+    if (!keyEl) {
+      return;
+    }
+    keyEl.classList.remove("pressed");
+
+    if (event.key === "Shift") {
+      this.isShifted = false;
+      this.element.classList.remove("shifted");
+    }
+
+    if (event.key === "CapsLock") {
+      this.isCapsLocked = false;
+      this.element.classList.remove("capslocked");
+    }
+  };
 
   renderGeneral() {
     this.element.innerHTML = templates.general();
--- templates.js
+++ templates.js
@@ -17,40 +17,40 @@
         <div class="key-row">
             <div class="dynamic-row row-0">
             </div>   
-            <button class="key-button delete-btn">delete</button>
+            <button class="key-button delete-btn" data-code="Backspace">delete</button>
         </div>
         <div class="key-row">
-            <button class="key-button tab-btn">tab</button>
+            <button class="key-button tab-btn" data-code="Tab">tab</button>
             <div class="dynamic-row row-1">                
             </div>           
         </div>
         <div class="key-row">
-            <button class="key-button caps-btn">caps lock</button>
+            <button class="key-button caps-btn" data-code="CapsLock">caps lock</button>
             <div class="dynamic-row row-2">                
             </div>
-        <button class="key-button enter-btn">enter</button>
+        <button class="key-button enter-btn" data-code="Enter">enter</button>
         </div>
         <div class="key-row">
-            <button class="key-button shift-btn">shift</button>
+            <button class="key-button shift-btn" data-code="ShiftLeft">shift</button>
             <div class="dynamic-row row-3">                
             </div>           
-            <button class="key-button shift-btn">shift</button>
+            <button class="key-button shift-btn" data-code="ShiftRight">shift</button>
         </div>
         <div class="key-row">
             <button class="key-button fn-btn">fn</button>
-            <button class="key-button control-btn">control</button>
-            <button class="key-button option-btn">option</button>
-            <button class="key-button command-btn">command</button>
-            <button class="key-button space-btn"></button>
-            <button class="key-button command-btn">command</button>
-            <button class="key-button option-btn">option</button>
+            <button class="key-button control-btn" data-code="ControlLeft">control</button>
+            <button class="key-button option-btn" data-code="AltLeft">option</button>
+            <button class="key-button command-btn" data-code="MetaLeft">command</button>
+            <button class="key-button space-btn" data-code="Space"></button>
+            <button class="key-button command-btn" data-code="MetaRight">command</button>
+            <button class="key-button option-btn" data-code="AltRight">option</button>
             <div class="arrow-wrap">
-                <button class="arrow-button">&larr;</button>
+                <button class="key-button arrow-btn" data-code="ArrowLeft">&larr;</button>
                 <div class="up-down-btn">
-                    <button class="arrow-button">&uarr;</button>
-                    <button class="arrow-button">&darr;</button>
+                    <button class="key-button arrow-btn" data-code="ArrowUp">&uarr;</button>
+                    <button class="key-button arrow-btn" data-code="ArrowDown">&darr;</button>
                 </div>
-                <button class="arrow-button">&rarr;</button>
+                <button class="key-button arrow-btn" data-code="ArrowRight">&rarr;</button>
             </div>
         </div>`;
     };
